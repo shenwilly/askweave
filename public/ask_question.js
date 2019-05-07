d1ed43905580b491823ed22f63a2aa951e58b5ea
willy <shen.willy95@gmail.com>
2019-05-08 00:41:41 +0700

    Updated ask question function

--- public/ask_question.js
+++ public/ask_question.js
@@ -1,5 +1,31 @@
 function ask_question () {
     (async () => {
-        
+        var question = $("#input_question").val();
+        var description = $("#input_description").val();
+        var unixTime = Math.round((new Date()).getTime() / 1000)
+
+        var data = {
+            'question': question,
+            'description': description,
+            'unix_time': unixTime
+        }
+
+        var tx =
+			await arweave.createTransaction(
+			    {
+			        data: JSON.stringify(data),
+			    },
+			    wallet
+			)
+
+        tx.addTag('App-Name', 'querweave')
+        tx.addTag('App-Version', versionNumber)
+        tx.addTag('Unix-Time', unixTime)
+        await arweave.transactions.sign(tx, wallet)
+        console.log(tx.id)
+        await arweave.transactions.post(tx)
+        alert('Question dispatched!')
+
+        $('#askQuestionModal').modal('hide');
     })()
 }
