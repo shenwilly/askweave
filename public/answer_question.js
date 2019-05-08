2935a792f1e7ad11179eecb77777b561bc016e58
willy <shen.willy95@gmail.com>
2019-05-08 22:21:12 +0700

    updated answer question function

--- public/answer_question.js
+++ public/answer_question.js
@@ -1,5 +1,29 @@
 function answer_question () {
     (async () => {
-        
+        var answer = $("#input_answer").val();
+        var unixTime = Math.round((new Date()).getTime() / 1000)
+
+        var data = {
+            'answer': answer,
+        }
+
+        var tx =
+            await arweave.createTransaction(
+                {
+                    data: JSON.stringify(data),
+                },
+                wallet
+            )
+
+        tx.addTag('App-Name', 'querweave')
+        tx.addTag('App-Version', versionNumber)
+        tx.addTag('Unix-Time', unixTime)
+        tx.addTag('Type', 'answer')
+        await arweave.transactions.sign(tx, wallet)
+        console.log(tx.id)
+        await arweave.transactions.post(tx)
+
+        $("#input_answer").val('');
+        alert('Answer dispatched!')
     })()
 }
