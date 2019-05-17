68e755a015f47c45c9103a2be196536ac3351204
willy <shen.willy95@gmail.com>
2019-05-17 20:06:43 +0700

    Added login indicator

--- js/login.js
+++ js/login.js
@@ -1,7 +1,11 @@
 function login (files) {
+    $(".file-input").hide();
+    $(".login-loading-indicator").show();
+
     var fr = new FileReader()
     fr.onload = function (ev) {
         try {
+
             wallet = JSON.parse(ev.target.result)
 
             var public_address;
@@ -9,12 +13,16 @@ function login (files) {
 				get_name(address).then((output_address) => {
 					public_address = output_address;
 					update_login_state(true, public_address);
-					$('#loginModal').modal('hide');
+                    
+                    $('#loginModal').modal('hide');
 				});
+
             });
         } catch (err) {
             alert('Error logging in: ' + err)
+        } finally {
         }
     }
     fr.readAsText(files[0])
+
 }
