ed3fd6b9c1c6fc72aa1fa11355b9e931cc0e221c
willy <shen.willy95@gmail.com>
2019-05-19 17:34:18 +0700

    Updated get question js

--- js/get_question.js
+++ js/get_question.js
@@ -1,79 +1,13 @@
-function get_question () {
+function get_question (question_id) {
     (async () => {
+        var tx = await this.arweave.transactions.get(question_id)
+        var jsonData = tx.get('data', {decode: true, string: true})
+        var data = JSON.parse(jsonData);
 
-        let query =
-			{
-			    op: 'and',
-			    expr1:
-					{
-                        op: 'and',
-                        expr1:
-                            {
-                                op: 'equals',
-                                expr1: 'App-Name',
-                                expr2: 'querweave'
-                            },
-                        expr2: 
-                            {
-                                op: 'equals',
-                                expr1: 'App-Version',
-                                expr2: versionNumber,
-                            }
-					},
-			    expr2:
-					{
-                        op: 'equals',
-                        expr1: 'Type',
-                        expr2: 'question'
-					},
-			}
+        var question = data["question"]
+        var description = data["description"]
 
-    	const res = await this.arweave.api.post(`arql`, query)
-        var tx_rows = []
-        if (res.data == '') {
-            tx_rows = []
-        } else {
-		    tx_rows = await Promise.all(res.data.map(async function (id, i) {
-                let tx_row = {}
-                var tx = await this.arweave.transactions.get(id)
-
-                tx_row['unixTime'] = '0'
-                tx.get('tags').forEach(tag => {
-                    let key = tag.get('name', { decode: true, string: true })
-                    let value = tag.get('value', { decode: true, string: true })
-                    if (key === 'Unix-Time') tx_row['unixTime'] = value
-                })
-
-                var jsonData = tx.get('data', {decode: true, string: true})
-                var data = JSON.parse(jsonData);
-
-                tx_row['id'] = id
-                tx_row['from'] = await get_name(await arweave.wallets.ownerToAddress(tx.owner))
-                tx_row['question'] = data["question"]
-                tx_row['description'] = data["description"]
-
-                return tx_row
-            }))
-        }
-
-        $(".loading-question").hide();
-        tx_rows.sort((a, b) => (Number(b.unixTime) - Number(a.unixTime)))
-        tx_rows.forEach(function (item) {
-            var question_card = $("#question-card-template").html()
-
-            var datetime = new Date(item["unixTime"]*1000);
-            var date_options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-            var formatted_datetime = datetime.toLocaleDateString('default', date_options)
-
-            question_card = question_card.replace("\[id\]", item["id"]);
-            question_card = question_card.replace("\[author\]", item["from"]);
-            question_card = question_card.replace("\[datetime\]", formatted_datetime);
-            question_card = question_card.replace("\[question\]", item["question"]);
-            question_card = question_card.replace("\[description\]", item["description"]);
-
-            $("#question-card-list").append(question_card);
-        })
-
-        mark_owned_questions()
+        $("#detail-page .question-title").html(question);
+        $("#detail-page .question-description").html(description);
     })()
 }
